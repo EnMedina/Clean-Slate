8e85090051cb18d263847f4cd4aabe27ad115e14
Enrique Medina <medina9304@gmail.com>
2019-09-13 15:36:26 -0400

    Updated version to have better log files

--- datascraper/boundaryGetter.py
+++ datascraper/boundaryGetter.py
@@ -1,35 +1,47 @@
-from urllib.request import urlopen,Request
+import requests
+import urllib3
 import logging
 import time
 import urllib.error
+import datetime
 
-boundaries = [50000, 40000,35000,30000,27500, 25000, 22500, 20000, 18000, 16000, 15000, 13000, 11500, 10000, 8000, 7500, 7000, 6500, 6000, 5500, 5000, 4500, 4000, 3500, 3250, 3000, 2750,2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 600, 500, 400, 300, 200, 100, 50, 25]
-logFile = open("boundaries.txt", "a")
+#Configuration to avoid SSL errors in servers
+requests.packages.urllib3.disable_warnings()
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += 'HIGH:!DH:!aNULL'
+try:
+    requests.packages.urllib3.contrib.pyopenssl.DEFAULT_SSL_CIPHER_LIST += 'HIGH:!DH:!aNULL'
+except AttributeError:
+   # no pyopenssl support used / needed / available
+   pass
+
+
+
+
+numAttempt = 1
+http = urllib3.PoolManager()
+logFile = open("boundaries.txt", "a+")
+workFile = open("workfile.txt", "a+")
 for courtNumber in range(2,68):
+    boundary = 30000
     courtString = format(courtNumber, "02d")
-    foundBound = False 
-    for num in boundaries:
-        for i in range(3):
-            crString = format(num - i, "07d")
-            print(f' Trying for CP-{courtString}-CR-{crString}-2018')
-            time.sleep(5)
-            try:    
-                file = f'https://services.pacourts.us/public/v1/cases/CP-{courtString}-CR-{crString}-2018'
-                req = Request(file,headers={'User-Agent': 'Mozilla/5.0'})
-                result = urlopen(req)
-                if result.getcode() == 200:
-                        logFile.write(f'CP {courtString}  was found at {crString}' + '\n')
-                        print("CR",courtString, " was found at " + crString)
-                        foundBound = True;
-                        break
-            except urllib.error.HTTPError as e:
-                if e.code == 404:
-                    continue
-                elif e.code == 429:
-                    time.sleep(3600)
-                    continue
-                else:
-                    raise
+    foundBound = False
+    while not foundBound and boundary > 300:
+        for i in range(2):
+             crString = format(boundary - i, "07d")
+             workFile.write(str(datetime.time.now()) + ' Attempt No.' +str(numAttempt) + f': Trying for CP-{courtString}-CR-{crString}-2018' + '\n')
+             numAttempt += 1
+             time.sleep(3)
+             webpage = http.request('GET', f'https://services.pacourts.us/public/v1/cases/CP-{courtString}-CR-{crString}-2018')
+             if webpage.status == 200 :
+                 logFile.write(f'CP {courtString}  was found at {crString}' + '\n')
+                 print("CR",courtString, " was found at " + crString)
+                 foundBound = True;
+                 break
+             elif webpage.status == 429:
+                 workFile.write('Reached request limit, sleeping for an hour' + '\n')
+                 time.sleep(3610)
+
         if foundBound:
-            break
+             break
+        boundary =  int(boundary//1.15)
 
